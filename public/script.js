5e93da6ba9a8cbdda34253e4750e4c8a0c868be3
keytmarq <b2028kmmcrisostomo@pshs.edu.ph>
2025-02-05 02:03:46 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -17,3 +17,71 @@ toggleRotationBtn.addEventListener('click', function() {
   }
   isRotating = !isRotating; // Toggle the state
 });
+
+
+const game = document.querySelector(".game");
+const levelOne = document.querySelector(".level-one");
+const levelTwo = document.querySelector(".level-two");
+const levelNumber = document.querySelector(".level-number");
+const description = document.querySelector(".description");
+const gameBtn = document.querySelector(".game-btn");
+
+function checkCollision(value) {
+  if (value.classList.contains("maze-border")) {
+    description.textContent = "You lose the game!";
+    game.style.pointerEvents = "none";
+    restartGame();
+  } else {
+    return 0;
+  }
+}
+
+function checkFinish(value) {
+  if (value.classList.contains("finish")) {
+    description.textContent =
+      "Congratulations! You win first level of the game.";
+    game.style.pointerEvents = "none";
+    startLevelTwo();
+  } else if (value.classList.contains("end-game")) {
+    description.textContent = "Congratulations! You win the game.";
+    game.style.pointerEvents = "none";
+    gameBtn.style.display = "inline-block";
+    gameBtn.textContent = "Play again!";
+    restartGame();
+  } else {
+    return 0;
+  }
+}
+
+window.addEventListener("mousemove", (e) => {
+  checkCollision(e.target);
+  checkFinish(e.target);
+});
+
+function restartGame() {
+  gameBtn.style.display = "inline-block";
+  gameBtn.textContent = "Play again!";
+  gameBtn.addEventListener("click", () => {
+    game.style.pointerEvents = "all";
+    gameBtn.style.display = "none";
+    levelOne.style.display = "block";
+    levelTwo.style.display = "none";
+    levelNumber.textContent = 1;
+    description.textContent =
+      "Make it to the finish line.You Should not touch the border of the maze.";
+  });
+}
+
+function startLevelTwo() {
+  gameBtn.style.display = "inline-block";
+  gameBtn.textContent = "Level 2";
+
+  gameBtn.addEventListener("click", () => {
+    game.style.pointerEvents = "all";
+    levelNumber.textContent = 2;
+    levelOne.style.display = "none";
+    levelTwo.style.display = "block";
+    description.textContent =
+      "Make it to the finish line.You Should not touch the border of the maze.";
+  });
+}
